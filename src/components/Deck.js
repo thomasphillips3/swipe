0ef9cc7351087d4f53dacde50f74ddbe55a051f9
Thomas Phillips <thomasphillips3@gmail.com>
2018-11-19 21:44:10 -0800

    Use PanResponder to drag this lil view around.

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { 
+    View
+    , Animated
+    , PanResponder
+ } from 'react-native';
 
 export class Deck extends Component{
+    constructor(props) {
+        super(props);
+
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({ 
+            onStartShouldSetPanResponder: () => true, 
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            }, 
+            onPanResponderRelease: () => {}
+         });
+
+         this.state = { panResponder, position };
+    }
+
     render() {
         return(
-            <View>
+            <Animated.View 
+                style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers} >
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         );
     }
 
