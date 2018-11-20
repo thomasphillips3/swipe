9ebba802ac1f8df7eb84f6f726ff971273995822
Thomas Phillips <thomasphillips3@gmail.com>
2018-11-19 18:25:24 -0800

    Initial commit.

---
+++ src/components/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Animated } fro 'react-native';
+
+export class Deck extends Component{
+    render() {
+        return(
+            <View></View>
+        );
+    }
+}
+
+export default Deck;
