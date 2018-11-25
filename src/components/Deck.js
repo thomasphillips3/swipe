4cf226e16f3ec94628e4f1d03a0ed00b70f732fa
Thomas Phillips <thomasphillips3@gmail.com>
2018-11-25 16:47:54 -0500

    Fix flashing after images load.

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -108,9 +108,9 @@ export class Deck extends Component {
             }
 
             return (
-                <View key={item.id} style={styles.cardStyle}>
+                <Animated.View key={item.id} style={styles.cardStyle}>
                     {this.props.renderCard(item)}
-                </View>
+                </Animated.View>
             );
         }).reverse();
     }
